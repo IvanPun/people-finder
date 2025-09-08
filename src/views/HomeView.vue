2d3d5eb3583a1ed8caeafad734518f7bf5b8be21
Ivan_Pun <whitetiger123asd@gmail.com>
2025-09-08 21:00:03 +0800

    update

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,26 +1,29 @@
 <template>
-  <div class="main" >
+  <div class="main">
     <div class="background-img">
       <img src="@/assets/images/background.jpg" alt="background" />
     </div>
 
     <!-- <div class="scroll-wrapper" :class="{ 'no-scroll': !searching }"> -->
-      <div class="main-body" :class="searching ? 'bottom-blank' : ''">
-        <div class="search-box">
-          <search v-model="keyword" @change="handleInput"></search>
+    <div class="main-body bottom-blank">
+      <div class="search-box">
+        <search v-model="keyword" @change="handleInput"></search>
 
-        </div>
-        <ResultArea v-if="searching" :keyword="keyword"></ResultArea>
       </div>
+      <ResultArea :keyword="keyword"></ResultArea>
+    </div>
     <!-- </div> -->
-
+    <transition name="fade">
+      <button v-show="showBtn" class="back-to-top" @click="scrollToTop">↑</button>
+    </transition>
   </div>
+
 </template>
 
 <script setup>
 import ResultArea from '@/components/ResultArea.vue';
 import search from '@/components/search.vue';
-import { ref } from 'vue';
+import { ref, onMounted } from 'vue';
 
 const searching = ref(false);
 
@@ -28,6 +31,21 @@ const keyword = ref('')
 const handleInput = (val) => {
   searching.value = val ? true : false
 }
+
+const showBtn = ref(false);
+
+const handleScroll = () => {
+  showBtn.value = window.scrollY > 200;
+};
+
+const scrollToTop = () => {
+  window.scrollTo({ top: 0, behavior: "smooth" });
+};
+
+onMounted(() => {
+  const el = document.querySelector(".scroll-wrapper") || window;
+  el.addEventListener("scroll", handleScroll);
+});
 </script>
 
 <style scoped>
@@ -82,12 +100,15 @@ const handleInput = (val) => {
 .scroll-wrapper {
   width: 100%;
   height: 100%;
-  overflow-y: auto;  /* 默認可滾動 */
+  overflow-y: auto;
+  /* 默認可滾動 */
 }
 
 .scroll-wrapper.no-scroll {
-  overflow: hidden;   /* 禁止滾動 */
-  touch-action: none; /* iOS防止手勢滾動 */
+  overflow: hidden;
+  /* 禁止滾動 */
+  touch-action: none;
+  /* iOS防止手勢滾動 */
 }
 
 
@@ -100,5 +121,35 @@ const handleInput = (val) => {
   display: flex;
   justify-content: center;
   margin-top: 5vh;
+}
+
+/* back to top btn */
+
+/* 進出場的過渡效果 */
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 1s ease;
+}
+
+/* 進場前 & 離場後的狀態 */
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
+.back-to-top {
+  position: fixed;
+  bottom: 40px;
+  right: 1rem;
+  background-color: #4CAF50;
+  color: white;
+  border: none;
+  border-radius: 50%;
+  width: 50px;
+  height: 50px;
+  font-size: 24px;
+  cursor: pointer;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.3);
+  transition: opacity 0.5s ease;
+  z-index: 9999;
 }
 </style>
