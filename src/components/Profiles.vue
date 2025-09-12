5f6cdb04702633a9410f475001692f55e96c2179
Ivan_Pun <whitetiger123asd@gmail.com>
2025-09-12 08:26:01 +0800

    Update Profiles.vue

--- src/components/Profiles.vue
+++ src/components/Profiles.vue
@@ -1,40 +1,39 @@
 <template>
     <div class="card">
         <div class="inside-card">
-            <div class="icon">
-                <el-image :src="profile.gender == '男' ? boyImg : girlImg" />
-            </div>
-            <div class="info">
-                <p>{{ profile.name }}</p>
-                <p>{{ profile.school }}</p>
-                <p>{{ profile.major }}</p>
-
-                <div class="tags">
-                    <p>愛好：</p>
-                    <tag v-for="value in profile.hobbies">{{ value }}</tag>
+            <div class="card-content">
+                <div class="icon">
+                    <el-image :src="profile.gender == '男' ? boyImg : girlImg" fit="contain" />
+                </div>
+                <div class="info">
+                    <p>{{ profile.name }}</p>
+                    <p>{{ profile.school }}</p>
+                    <p>{{ profile.major }}</p>
+                    <div class="tags">
+                        <p style="font-size: 1rem;">愛好：</p>
+                        <tag v-for="value in profile.hobbies">{{ value }}</tag>
+                    </div>
                 </div>
             </div>
             <!-- <el-row :gutter="20">
-                <el-col :span="8">
-                    <el-image :src="profile.gender == '男' ? boyImg : girlImg" />
+                <el-col :span="auto">
+                    <div class="icon">
+                        <el-image :src="profile.gender == '男' ? boyImg : girlImg" fit="contain" />
+                    </div>
                 </el-col>
                 <el-col :span="16">
                     <div class="info">
                         <p>{{ profile.name }}</p>
-                        <p style="font-size: 1rem; margin-top: 0.5rem;">{{ profile.school }}</p>
-                        <p style="font-size: 1rem;">{{ profile.major }}</p>
-                        <p style="font-size: 1rem;">愛好：</p>
+                        <p>{{ profile.school }}</p>
+                        <p>{{ profile.major }}</p>
                         <div class="tags">
+                            <p style="font-size: 1rem;">愛好：</p>
                             <tag v-for="value in profile.hobbies">{{ value }}</tag>
                         </div>
                     </div>
-
-
                 </el-col>
             </el-row> -->
         </div>
-
-
     </div>
 </template>
 
@@ -52,13 +51,12 @@ const girlImg = "/images/girl.jpg"
 <style scoped>
 .card {
     width: 90vw;
-    height: 20vh;
     margin-top: 2.5vh;
 
     /* 半透明玻璃卡片 */
     background: rgba(255, 255, 255, 0.72);
     border: 1px solid rgba(255, 255, 255, 0.3);
-    padding: 3%;
+    padding: 10px;
 
     border-radius: 15px;
     box-shadow: 0 8px 32px rgba(0, 0, 0, 0.2);
@@ -66,31 +64,27 @@ const girlImg = "/images/girl.jpg"
 
 .inside-card {
     width: 100%;
-    height: 100%;
     background: rgba(194, 181, 155, 0.55);
     border-radius: 10px;
-    padding: 2.5% 4%;
+    padding: 10px 20px;
 
     display: flex;
+    flex-direction: column;
+    overflow: hidden;
+    box-sizing: border-box;
+}
+
+.card-content {
+  display: flex;
+  align-items: center;
 }
 
 .icon {
-    height: 100%;
-    display: flex;
-    align-items: center;
-    overflow: hidden;
-    border-radius: 15px;
-    flex-shrink: 0;
+  flex-shrink: 0;
+  height: 15vh;
 }
 
 .icon .el-image {
-    width: auto;
-    height: 100%;
-    border-radius: 15px;
-    object-fit: contain;
-}
-
-.icon .el-image img {
   height: 100%;
   width: auto;
   object-fit: contain;
@@ -103,6 +97,7 @@ const girlImg = "/images/girl.jpg"
     display: flex;
     flex-direction: column;
     justify-content: space-between;
+
 }
 
 .info p {
